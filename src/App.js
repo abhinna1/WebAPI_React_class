d251b3a6ecf47f24b47f0fa34690bb0397370ae0
abhinna1 <manandharabhinna@gmail.com>
2023-01-05 08:54:57 +0545

    updated readme.md

--- src/App.js
+++ src/App.js
@@ -1,22 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+
 
 function App() {
+  const [count, setCount] = useState(0);
+  // setTimeout(
+  //   ()=>{
+  //     setCount(count+1);
+  //     console.log(count);
+  //   }, 1000
+  // )
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          {count}
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <button onClick={()=>{setCount(count+1)}}>Increment</button>
       </header>
     </div>
   );
