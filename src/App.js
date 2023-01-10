77eb8bd27fbb51592f10e2ee7e2c982e58438e7e
abhinna1 <manandharabhinna@gmail.com>
2023-01-10 08:38:55 +0545

    db file added.

--- src/App.js
+++ src/App.js
@@ -1,24 +1,11 @@
-import { useState } from "react";
-
+import ReviewCounter from "./Component/UseStateClass/ReviewCounter";
 
 function App() {
-  const [count, setCount] = useState(0);
-  // setTimeout(
-  //   ()=>{
-  //     setCount(count+1);
-  //     console.log(count);
-  //   }, 1000
-  // )
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          {count}
-        </p>
-        <button onClick={()=>{setCount(count+1)}}>Increment</button>
-      </header>
-    </div>
-  );
+    <>
+      <h2>Notes</h2>
+    </>  );
 }
 
 export default App;
