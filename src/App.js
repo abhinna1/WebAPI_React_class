ac1dc304e8f09b2ac73e2fbe10a4753d8fc08366
abhinna1 <manandharabhinna@gmail.com>
2023-01-17 09:17:48 +0545

    delete task handled.

--- src/App.js
+++ src/App.js
@@ -1,36 +1,64 @@
 import { useState, useEffect } from "react";
-import axios from 'axios';
+import axios from "axios";
 
 function App() {
-
   const [notes, setNotes] = useState([]);
   const [newNote, setNewNote] = useState("");
   const [showAll, setShowAll] = useState(true);
 
-
-  useEffect(()=>{
-    axios.get('http://localhost:3001/notes')
-    .then(res=>{
-      setNotes(res.data)
-    })
-    .catch(err=>{console.log(err)});
-  }, [])
+  useEffect(() => {
+    axios
+      .get("http://localhost:3001/notes")
+      .then((res) => {
+        setNotes(res.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
   // Handle form submit.
   const handleSubmit = (e) => {
     e.preventDefault();
+
     const note = {
-      id: notes.length + 1,
+      // id: notes.length + 1,
       content: newNote,
       date: new Date().toISOString(),
       important: Math.random() < 0.5,
     };
-    setNotes(notes.concat(note));
+
+    if (newNote !== "") {
+      axios
+        .post("http://localhost:3001/notes", note)
+        .then((response) => {
+          console.log(response);
+          setNotes(notes.concat(response.data));
+          setNewNote("");
+        })
+        .catch((err) => console.log(err));
+    }
   };
 
   // Handle change in form input.
   const handleChange = (e) => {
     console.log(e.target.value);
     setNewNote(e.target.value);
+  };
+
+  // Handle task delete.
+  const handleDelete = async (id) => {
+    try {
+      await axios.delete(`http://localhost:3001/notes/${id}`);
+      setNotes(notes.filter((n) => n.id !== id));
+    } catch (e) {
+      console.log(e);
+    }
+
+    // .then(response => {
+    //   console.log(response)
+    //   setNotes(notes.filter(n => n.id !== id ))
+    // })
+    // .catch(err => console.log(err))
   };
 
   return (
@@ -50,7 +78,13 @@ function App() {
             <li key={note.id}>
               <strong>{note.content}</strong>
               <p>{note.date}</p>
-              <button>Delete</button>
+              <button
+                onClick={() => {
+                  handleDelete(note.id);
+                }}
+              >
+                Delete
+              </button>
               <hr />
             </li>
           );
